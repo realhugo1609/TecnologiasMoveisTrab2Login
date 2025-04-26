300e02c38932924862ad841fd35ebbb201e6d198
realhugo1609 <realhugo1609@gmail.com>
2025-04-26 05:38:06 -0300

    identifica login e senha, mas nao conecta na rede indicada

--- src/main.cpp
+++ src/main.cpp
@@ -1,190 +1,187 @@
-#define BUILTIN_LED 2
-
+// Load Wi-Fi library
 #include <WiFi.h>
-#include <PubSubClient.h>
 
-#include "ESPAsyncWebServer.h"
-#include "SPIFFS.h"
+// Replace with your network credentials
+const char* ssid     = "ESP32-Access-Point";
+const char* password = "123456789";
 
+// Set web server port number to 80
+WiFiServer server(80);
 
-// Update these with values suitable for your network.
+// Variable to store the HTTP request
+String header;
 
-const char* ssid = "LEO308_2G";
-const char* password = "14393018";
+// Auxiliar variables to store the current output state
+String output26State = "off";
+String output27State = "off";
 
-const char* mqtt_server = "broker.emqx.io";
-
-// Set LED GPIO
-const int ledPin = 2;
-// Stores LED state
-String ledState;
-
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
+// Assign output variables to GPIO pins
+const int output26 = 26;
+const int output27 = 27;
 
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE	(50)
-char msg[MSG_BUFFER_SIZE];
-int value = 0;
-
-String processor(const String& var)
+String meuLogin(String texto)
 {
-  Serial.println(var);
-  if(var == "STATE")
-  {
-    ledState = "MACARRAO";
-    Serial.println("testano");
-    return ledState;
-  }
-  return String();
+  int indexInicialLogin = texto.indexOf("login=")+6; //+6 é o tamanho de 'login='
+  if (texto.indexOf("login=") == -1) return "nenhum";
+  int indexEComercial = texto.indexOf("&senha");
+  int indexInicialsenha = indexEComercial + 7; //+7 é o tamanho de '&senha='
+  int indexFinalizador = texto.indexOf("&finalizador");
+  return texto.substring(indexInicialLogin, indexEComercial);
 }
-
-//void setup_wifi(const char* ssid, const char* password) {
-void setup_wifi(const String ssid, const String password) {
-
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  randomSeed(micros());
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
-      // ... and resubscribe
-      client.subscribe("inTopic");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
+String minhaSenha(String texto)
+{
+  int indexInicialLogin = texto.indexOf("login=")+6; //+6 é o tamanho de 'login='
+  int indexEComercial = texto.indexOf("&senha");
+  int indexInicialsenha = indexEComercial + 7; //+7 é o tamanho de '&senha='
+  int indexFinalizador = texto.indexOf("&finalizador");
+  return texto.substring(indexInicialsenha, indexFinalizador);
 }
 
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
-  setup_wifi(ssid, password);
+  // Initialize the output variables as outputs
+  pinMode(output26, OUTPUT);
+  pinMode(output27, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output26, LOW);
+  digitalWrite(output27, LOW);
 
-  //client.setServer(mqtt_server, 1883);
-  //client.setCallback(callback);
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid, password);
 
-
-  // Initialize SPIFFS
-  if(!SPIFFS.begin(true)){
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
-
-
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-  // Route to load style.css file
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/style.css", "text/css");
-  });
-  // Route to set GPIO to HIGH
-  server.on("/retorno", HTTP_GET, [](AsyncWebServerRequest *request){
-    digitalWrite(ledPin, HIGH); 
-    AsyncWebParameter* l = request->getParam(0); //LOGIN
-    AsyncWebParameter* s = request->getParam(1); //SENHA
-    /*
-    if (((l->value() == "hugo") && (s->value() == "1234"))  ||
-       ((l->value() == "mikael") && (s->value() == "2025")))  request->send(SPIFFS, "/sucessologin.html", String(), false, processor); 
-    else request->send(SPIFFS, "/falhalogin.html", String(), false, processor); 
-    */
-   request->send(SPIFFS, "/index.html", String(), false, processor);
-
-    WiFi.disconnect();
-    while (WiFi.status() != WL_DISCONNECTED) 
-    {
-      delay(500);
-      Serial.print("d...");
-    }
-    Serial.println("Status:");
-    Serial.println(WiFi.status());
-    setup_wifi(l->value(), s->value());
-
-  });
-  // Route to set GPIO to LOW
-  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
-    digitalWrite(ledPin, LOW);    
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });  
-
-  // Start server
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+  
   server.begin();
-
 }
 
-void loop() {
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
 
-/*
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            if (meuLogin(header) != "nenhum")
+            {
+              Serial.println("teste LEOOOO");
+              Serial.println("login:");
+              Serial.println(    meuLogin(header).length()  );
+              Serial.println("senha:");
+              Serial.println(    minhaSenha(header).length()   );
+            }
 
-  unsigned long now = millis();
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    ++value;
-    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("outTopic", msg);
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /26/on") >= 0) {
+              Serial.println("GPIO 26 on");
+              output26State = "on";
+              digitalWrite(output26, HIGH);
+            } else if (header.indexOf("GET /26/off") >= 0) {
+              Serial.println("GPIO 26 off");
+              output26State = "off";
+              digitalWrite(output26, LOW);
+            } else if (header.indexOf("GET /27/on") >= 0) {
+              Serial.println("GPIO 27 on");
+              output27State = "on";
+              digitalWrite(output27, HIGH);
+            } else if (header.indexOf("GET /27/off") >= 0) {
+              Serial.println("GPIO 27 off");
+              output27State = "off";
+              digitalWrite(output27, LOW);
+            }
+            
+            client.println("<!DOCTYPE html><html>");
+            client.println("<html><body><fieldset style='display: inline-block'>");
+            client.println("<legend>Login</legend>");
+            client.println("<form action='' method='get' target='_blank'>");
+            client.println("<label for='login'>Login:</label>");
+            client.println("<input type='text' id='login' name='login'><br><br>");
+            client.println("<label for='senha'>Senha:</label>");
+            client.println("<input type='password' id='senha' name='senha'><br><br>");
+            client.println("<input type='text' id='finalizador' name='finalizador' style='display:none'><br><br>");
+            client.println("<input type='submit' value='LOGIN'>");
+            client.println("</form></fieldset></body></html>");
+            // The HTTP response ends with another blank line
+            client.println();
+
+
+
+
+
+            /*
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP32 Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 26  
+            client.println("<p>GPIO 26 - State " + output26State + "</p>");
+            // If the output26State is off, it displays the ON button       
+            if (output26State=="off") {
+              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            // Display current state, and ON/OFF buttons for GPIO 27  
+            client.println("<p>GPIO 27 - State " + output27State + "</p>");
+            // If the output27State is off, it displays the ON button       
+            if (output27State=="off") {
+              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            */
+
+
+
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
-*/
 }
