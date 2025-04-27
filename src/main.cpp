ec5eb35ae541c83c07fa8ea3b2f0f9e70be480ec
realhugo1609 <realhugo1609@gmail.com>
2025-04-26 21:37:20 -0300

    Access poins, login e MQTT funcionando

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 
 #include <WiFi.h>
+#include <PubSubClient.h>
+
+int flagAntesDepoisLogin = 1;
 
 // Replace with your network credentials
 const char* ssid     = "ESP32-Access-Point";
 const char* password = "123456789";
+
+//MQTT ADICOES
+const char* mqtt_server = "broker.emqx.io";   
+WiFiClient espClient;
+PubSubClient clientMQTT(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
 
 // Set web server port number to 80
 WiFiServer server(80);
@@ -59,10 +72,61 @@ void setup_wifi(const String ssid, const String password)
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   
+  flagAntesDepoisLogin = 2; //COMECA O MQTT
+}
+
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    //digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    //digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!clientMQTT.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (clientMQTT.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      //client.publish("outTopic", "hello world");   //TIREI... NAO SEI SE PRECISA SE INSCREVER
+      // ... and resubscribe
+      clientMQTT.subscribe("terroso/mensagens");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(clientMQTT.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
 
 void setup() {
   Serial.begin(115200);
+
+  //MQTT ADICOES
+  clientMQTT.setServer(mqtt_server, 1883);
+  clientMQTT.setCallback(callback);
+
   // Initialize the output variables as outputs
   pinMode(output26, OUTPUT);
   pinMode(output27, OUTPUT);
@@ -83,91 +147,104 @@ void setup() {
 }
 
 void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
+  if (flagAntesDepoisLogin == 1)
+  {
+    WiFiClient client = server.available();   // Listen for incoming clients
+    if (client) {                             // If a new client connects,
+      Serial.println("New Client.");          // print a message out in the serial port
+      String currentLine = "";                // make a String to hold incoming data from the client
+      while (client.connected()) {            // loop while the client's connected
+        if (client.available()) {             // if there's bytes to read from the client,
+          char c = client.read();             // read a byte, then
+          Serial.write(c);                    // print it out the serial monitor
+          header += c;
+          if (c == '\n') {                    // if the byte is a newline character
+            // if the current line is blank, you got two newline characters in a row.
+            // that's the end of the client HTTP request, so send a response:
+            if (currentLine.length() == 0) {
+              // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+              // and a content-type so the client knows what's coming, then a blank line:
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println("Connection: close");
+              client.println();
+              
+              if (meuLogin(header) != "nenhum")
+              {
+                Serial.println("teste LEOOOO");
+                Serial.println("login:");
+                Serial.println(    meuLogin(header).length()  );
+                Serial.println("senha:");
+                Serial.println(    minhaSenha(header).length()   );
+              }
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            if (meuLogin(header) != "nenhum")
-            {
-              Serial.println("teste LEOOOO");
-              Serial.println("login:");
-              Serial.println(    meuLogin(header).length()  );
-              Serial.println("senha:");
-              Serial.println(    minhaSenha(header).length()   );
+              // turns the GPIOs on and off
+              if (header.indexOf("GET /26/on") >= 0) {
+                Serial.println("GPIO 26 on");
+                output26State = "on";
+                digitalWrite(output26, HIGH);
+              } else if (header.indexOf("GET /26/off") >= 0) {
+                Serial.println("GPIO 26 off");
+                output26State = "off";
+                digitalWrite(output26, LOW);
+              } else if (header.indexOf("GET /27/on") >= 0) {
+                Serial.println("GPIO 27 on");
+                output27State = "on";
+                digitalWrite(output27, HIGH);
+              } else if (header.indexOf("GET /27/off") >= 0) {
+                Serial.println("GPIO 27 off");
+                output27State = "off";
+                digitalWrite(output27, LOW);
+              }
+              
+              client.println("<!DOCTYPE html><html>");
+              client.println("<html><body><fieldset style='display: inline-block'>");
+              client.println("<legend>Login</legend>");
+              client.println("<form action='' method='get' target='_blank'>");
+              client.println("<label for='login'>Login:</label>");
+              client.println("<input type='text' id='login' name='login'><br><br>");
+              client.println("<label for='senha'>Senha:</label>");
+              client.println("<input type='password' id='senha' name='senha'><br><br>");
+              client.println("<input type='text' id='finalizador' name='finalizador' style='display:none'><br><br>");
+              client.println("<input type='submit' value='LOGIN'>");
+              client.println("</form></fieldset></body></html>");
+              // The HTTP response ends with another blank line
+              client.println();
+
+              // Break out of the while loop
+              break;
+            } else { // if you got a newline, then clear currentLine
+              currentLine = "";
             }
-
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /26/on") >= 0) {
-              Serial.println("GPIO 26 on");
-              output26State = "on";
-              digitalWrite(output26, HIGH);
-            } else if (header.indexOf("GET /26/off") >= 0) {
-              Serial.println("GPIO 26 off");
-              output26State = "off";
-              digitalWrite(output26, LOW);
-            } else if (header.indexOf("GET /27/on") >= 0) {
-              Serial.println("GPIO 27 on");
-              output27State = "on";
-              digitalWrite(output27, HIGH);
-            } else if (header.indexOf("GET /27/off") >= 0) {
-              Serial.println("GPIO 27 off");
-              output27State = "off";
-              digitalWrite(output27, LOW);
-            }
-            
-            client.println("<!DOCTYPE html><html>");
-            client.println("<html><body><fieldset style='display: inline-block'>");
-            client.println("<legend>Login</legend>");
-            client.println("<form action='' method='get' target='_blank'>");
-            client.println("<label for='login'>Login:</label>");
-            client.println("<input type='text' id='login' name='login'><br><br>");
-            client.println("<label for='senha'>Senha:</label>");
-            client.println("<input type='password' id='senha' name='senha'><br><br>");
-            client.println("<input type='text' id='finalizador' name='finalizador' style='display:none'><br><br>");
-            client.println("<input type='submit' value='LOGIN'>");
-            client.println("</form></fieldset></body></html>");
-            // The HTTP response ends with another blank line
-            client.println();
-
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
+          } else if (c != '\r') {  // if you got anything else but a carriage return character,
+            currentLine += c;      // add it to the end of the currentLine
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
         }
       }
+
+      // Close the connection
+      client.stop();
+      Serial.println("Client disconnected.");
+      Serial.println("");
+
+      if (meuLogin(header) != "nenhum")
+      {
+        setup_wifi(meuLogin(header), minhaSenha(header));
+      }
+
+      // Clear the header variable
+      header = "";
     }
 
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-
-    if (meuLogin(header) != "nenhum")
-    {
-      setup_wifi(meuLogin(header), minhaSenha(header));
-    }
-
-    // Clear the header variable
-    header = "";
   }
+
+  else if (flagAntesDepoisLogin == 2)
+  {
+    //MQTT ADICOES
+    if (!clientMQTT.connected()) {
+      reconnect();
+    }
+    clientMQTT.loop();
+  }
+  
 }
